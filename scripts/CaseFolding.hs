cd4580729feb36c534d5921d8337b4bfa133be88
Bodigrim <andrew.lelechenko@gmail.com>
2021-07-18 20:56:40 +0100

    Experiment with case conversions

--- scripts/CaseFolding.hs
+++ scripts/CaseFolding.hs
@@ -11,6 +11,7 @@ module CaseFolding
     ) where
 
 import Arsec
+import Data.Bits
 
 data Fold = Fold {
       code :: Char
@@ -36,11 +37,12 @@ parseCF name = parse entries name <$> readFile name
 mapCF :: CaseFolding -> [String]
 mapCF (CF _ ms) = typ ++ (map nice . filter p $ ms) ++ [last]
   where
-    typ = ["foldMapping :: forall s. Char -> s -> Step (CC s) Char"
-           ,"{-# NOINLINE foldMapping #-}"]
-    last = "foldMapping c s = Yield (toLower c) (CC s '\\0' '\\0')"
-    nice c = "-- " ++ name c ++ "\n" ++
-             "foldMapping " ++ showC (code c) ++ " s = Yield " ++ x ++ " (CC s " ++ y ++ " " ++ z ++ ")"
-       where [x,y,z] = (map showC . take 3) (mapping c ++ repeat '\0')
+    typ = ["foldMapping :: Char# -> _"
+           ,"{-# NOINLINE foldMapping #-}"
+           ,"foldMapping = \\case"]
+    last = "  _ -> unI64 0"
+    nice c = "  -- " ++ name c ++ "\n" ++
+             "  " ++ showC (code c) ++ "# -> unI64 "  ++ show (ord x + (ord y `shiftL` 21) + (ord z `shiftL` 42))
+       where x:y:z:_ = mapping c ++ repeat '\0'
     p f = status f `elem` "CF" &&
           mapping f /= [toLower (code f)]
